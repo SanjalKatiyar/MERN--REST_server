b31cf6f5325049c63636bd1a7140dedc8d71efb9
SanjalKatiyar <sanjaldhir@gmail.com>
2018-11-02 09:53:25 +0530

    Cors

---
+++ routes/cors.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+//whitelist contains all the origins server is willing to accept
+const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
+var corsOptionsDelegate = (req, callback) => {
+    var corsOptions;
+    console.log(req.header('Origin'));
+    if(whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true };
+    }
+    else {
+        corsOptions = { origin: false };
+    }
+    callback(null, corsOptions);
+};
+
+exports.cors = cors();
+exports.corsWithOptions = cors(corsOptionsDelegate);
