3599d6cee88a8a65441634ba32cccec1e2718639
SanjalKatiyar <sanjaldhir@gmail.com>
2018-11-07 00:27:01 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '249381372395744',
+        clientSecret: '0a378da18d5f1f545cca4b2509db2b46'
+    }
 }
