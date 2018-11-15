d677129425378b85c3d47ff65cb44795246de34f
SanjalKatiyar <sanjaldhir@gmail.com>
2018-11-15 17:56:03 +0530

    conflict

--- routes/cors.js
+++ routes/cors.js
@@ -3,11 +3,7 @@ const cors = require('cors');
 const app = express();
 
 //whitelist contains all the origins server is willing to accept
-<<<<<<< HEAD
 var whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://LAPTOP-A7T3IMMU:3001'] ;
-=======
-var whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200']
->>>>>>> d08add7c785ad1f66d07b50dec90d191dc1b50a6
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
